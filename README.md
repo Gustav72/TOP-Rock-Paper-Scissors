181cb9fe9aceed1568a571eb12c8decef7fa0942
Wolfgang <wolfgangkirstein7@gmail.com>
2022-10-24 08:27:14 -0400

    creating html elements and styling choices

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # TOP-Rock-Paper-Scissors
+
 The Odin Project Rock Paper Scissors Project
+
+Now working on adding a user interface.
--- script.js
+++ script.js
@@ -7,44 +7,41 @@ let computerScore = 0;
 function getComputerChoice() {
     let x = Math.random()*100;
     console.log(x);
-    if (x <= 33) return 'Rock';
-    else if (x <= 66) return 'Paper';
-    else return 'Scissors';
+    if (x <= 33) return 'rock';
+    else if (x <= 66) return 'paper';
+    else return 'scissors';
 }
 
 computerSelection = getComputerChoice();
-function getPlayerChoice() {
-   return window.prompt("Rock Paper or Scissors?")
-}
 
-game(playerSelection, computerSelection);
+//game(playerSelection, computerSelection);
 
 function playRound(playerSelection, computerSelection) {    
 
     if (playerSelection === computerSelection) {
          result = "Its a tie!";
     }
-    else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+    else if (playerSelection == 'rock' && computerSelection == 'scissors') {
         result = 'You win! Rock beats scissors'; 
         playerScore++;
     }
-    else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
+    else if (playerSelection == 'rock' && computerSelection == 'paper') {
        result = 'You Lose! Paper beats rock'
        computerScore += 1;
     }
-    else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+    else if (playerSelection == 'paper' && computerSelection == 'rock') {
         result = 'You Win! Paper beats rock'
         playerScore++;
     }
-    else if (playerSelection == 'Paper' && computerSelection == 'Paper') {
+    else if (playerSelection == 'paper' && computerSelection == 'paper') {
         result = 'You Lose! Paper beats rock'
         computerScore += 1;
     }
-    else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
+    else if (playerSelection == 'scissors' && computerSelection == 'paper') {
         result = 'You Win! Scissors beats paper'
         playerScore++;
     }
-    else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
+    else if (playerSelection == 'scissors' && computerSelection == 'rock') {
         result = 'You Lose! Rock beats scissors'
         computerScore += 1;
     }
@@ -58,12 +55,12 @@ function game(playerSelection, computerSelection) {
 
     while(playerScore < 5 && computerScore < 5) {
         computerSelection = getComputerChoice();
-        playerSelection = window.prompt("Rock Paper or Scissors")
+        //playerSelection = window.prompt("Rock Paper or Scissors")
         playRound(playerSelection, computerSelection);
         console.log("Player: " + playerSelection);
         console.log("Computer: " + computerSelection);
-        console.log("Players Score : " + playerScore)
-        console.log("Computer Score : " + computerScore)
+        console.log("Players Score: " + playerScore)
+        console.log("Computer Score: " + computerScore)
         console.log(result);
     }
 }
